fa98c2df735b37b5282cdb2b178d736985537ab0
jadey1223 <142165000+jadey1223@users.noreply.github.com>
2024-04-08 18:49:24 +0100

    PHP form handling for booking.html

--- process-form.php
+++ process-form.php
@@ -1,4 +1,17 @@
-<?php
-    print_r($_POST);
+
+<?php 
+
+$firstname = $_POST["firstName"];
+$lastName = $_POST["lastName"];
+$email = $_POST["email"];
+$number = $_POST["number"];
+$comment = $_POST["comment"];
+$time = $_POST["time"];
+$people = $_POST["people"];
+
+
+
+var_dump($firtname,$lastName,$email,$number,$comment,$people,$time);
+
 
 
