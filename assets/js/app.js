cdcdc3f0c3b4eff4333646d2dcd05fef28f03f55
jadey1223 <142165000+jadey1223@users.noreply.github.com>
2024-05-08 20:18:54 +0100

    google authentication required

---
+++ assets/js/app.js
@@ -0,0 +1,4 @@
+document.getElementById('closebtn').addEventListener('click', function() {
+    var navbar = document.getElementById('overlay-content');
+    navbar.classList.toggle('active');
+  });
