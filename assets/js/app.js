a41141d4c451e7bf79d46396e1f66cbad1a2169a
jadey1223 <142165000+jadey1223@users.noreply.github.com>
2024-05-12 16:03:37 +0100

    google auth0 sign in link

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,14 @@
-document.getElementById('closebtn').addEventListener('click', function() {
-    var navbar = document.getElementById('overlay-content');
-    navbar.classList.toggle('active');
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
   });
+}
