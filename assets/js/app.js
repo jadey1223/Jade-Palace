c5be6cc4e586190a34cc384116bdefa6710c6728
jadey1223 <142165000+jadey1223@users.noreply.github.com>
2024-05-12 17:43:37 +0100

    Google auth0 configured

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
+
+
+
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
@@ -12,3 +15,22 @@ function signOut() {
     console.log('User signed out.');
   });
 }
+
+function onSignIn(googleUser) {
+  var id_token = googleUser.getAuthResponse().id_token;
+}
+
+gapi.load('auth2', function() {
+  gapi.auth2.init({
+    client_id: '444189995200-3r9lni89ot0e7nchd82bhecnv2mftnlf.apps.googleusercontent.com',
+    scope: 'https://www.googleapis.com/auth/spreadsheets',
+  });
+});
+
+var xhr = new XMLHttpRequest();
+xhr.open('POST', 'http://localhost:3000');
+xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+xhr.onload = function() {
+  console.log('Signed in as: ' + xhr.responseText);
+};
+xhr.send('idtoken=' + id_token);
