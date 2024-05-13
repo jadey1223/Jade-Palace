9a7e63cc78ec1e870d3384c418bba1d8103a15c5
jadey1223 <142165000+jadey1223@users.noreply.github.com>
2024-05-13 20:56:56 +0100

    Implemented Google authentication button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,3 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
 
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
